a069df970be808ac9f45b47bd445c01277630d15
Laurent Denoue <ldenoue@google.com>
2023-03-01 06:49:31 -0800

    compiles

---
+++ mouse-events.js
@@ -0,0 +1,22 @@
+var respecConfig = {
+  specStatus: "unofficial",
+  github: {
+    repoURL: "https://github.com/screen-share/mouse-events",
+    branch: "master",
+  },
+  editors: [
+    {
+      name: "Elad Alon",
+      email: "eladalon@google.com",
+      company: "Google",
+    },
+    {
+      name: "Laurent Denoue",
+      email: "ldenoue@google.com",
+      company: "Google",
+    },
+  ],
+  xref: ["html", "infra", "permissions", "permissions-policy", "dom", "mediacapture-streams", "webidl", "screen-capture"],
+  shortName: "mouse-events",
+  subjectPrefix: "[mouse-events]",
+};
